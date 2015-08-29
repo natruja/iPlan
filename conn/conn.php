1a555e840c2a4d66cc132fc970331136d87b6e17
Natruja <plelek69@gmail.com>
2015-08-29 22:20:18 +0700

    Add conn db

---
+++ conn/conn.php
@@ -0,0 +1,13 @@
+<?php 
+ try{
+ 	$handler = new PDO('mysql:host=127.0.0.1;dbname=app', 'root', '');
+ 	$handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+ }catch(PDOException $e){
+ 	 echo $e->getMessage();
+ 	 die();
+ }
+ 
+
+
+?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PDO Test</title>
+</head>
+<body>
+	<?php 
+		
+
+	?>
+</body>
+</html>
